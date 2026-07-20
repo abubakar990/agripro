005c27883f92f475628a414655f20a406e1812fb
Abu Bakar <ch.abubakar998@gmail.com>
2026-07-20 10:16:01 +0500

    fix: resolve empty dashboard stats by fixing globalStore dateRange default

--- agripro-web/src/store/globalStore.js
+++ agripro-web/src/store/globalStore.js
@@ -4,7 +4,7 @@ export const useGlobalStore = create((set) => ({
   farmFilter: 'all',
   setFarmFilter: (filter) => set({ farmFilter: filter }),
   
-  dateRange: 'this_month',
+  dateRange: '30d',
   setDateRange: (range) => set({ dateRange: range }),
   
   customRange: { start: '', end: '' },
